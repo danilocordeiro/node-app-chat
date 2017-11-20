5d3621187f81e461092563eb3094677ce84eea28
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2017-11-20 11:29:11 -0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ const socket = io();
 socket.on('connect', function() {
 	console.log('Connnected to server');
 	
-	socket.emit('createMessage', {
-		from: 'Andrew',
-		text: 'Yup, thate work'
-	});
-
 	socket.on('disconnect', function() {
 		console.log('Disconnected from server');
 	});
